ae6fbf2c0a59f0fb0d55acabc2c57e309f76f8e7
Ayushman Singh <Ayushman12700singh@gmail.com>
2025-11-18 13:23:22 +0530

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["randomuser.me"], // <- add here
   },
+  // ADD THIS experimental block
+  experimental: {
+    outputFileTracingIncludes: {
+      '/*': [
+        './node_modules/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node',
+        './node_modules/.prisma/client/schema.prisma',
+      ],
+    },
+  },
 }
 
 export default nextConfig
